0d55e61d6804a7f4924e06c2eb037de00eb3e510
vfedotova418-png <vfedotova418@gmail.com>
2026-03-28 16:18:00 +0300

    Conversion function added

--- converter/main.js
+++ converter/main.js
@@ -1,12 +1,17 @@
-const amount = document.getElementById("amount").value;
-const currency = document.getElementById("currency").value;
 const convertBtn = document.getElementById("convertBtn");
 const result = document.getElementById("result");
 
 convertBtn.addEventListener("click", () => {
-    if (amount === "" || amount === null) {
-        result.textContent = "Ошибка: Введите сумму!"
+    let amount = document.getElementById("amount").value;
+    let currency = document.getElementById("currency").value;
+    if (amount === "" || amount === null || amount === "0") {
+        result.textContent = "Ошибка: Некорректная сумма"
     } else {
-        result.textContent = amount;
+        if (currency === "EUR") {
+            result.textContent = `Итог: ${(amount * 0.0107).toFixed(4)} ${currency}`;
+        }
+        if (currency === "USD") {
+            result.textContent = `Итог: ${(amount * 0.0123).toFixed(4)} ${currency}`;
+        }
     }
 });
